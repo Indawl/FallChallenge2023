619d11baaf983820d2b3c5ab6f83a2b2841f0c15
Indawl <Indawl@INDAWLLAND>
2023-12-24 02:02:07 +0300

    1

--- FallChallenge2023/Bots/Bronze/Agents/Decisions/SearchDecision.cs
+++ FallChallenge2023/Bots/Bronze/Agents/Decisions/SearchDecision.cs
@@ -1,4 +1,5 @@
 ﻿using FallChallenge2023.Bots.Bronze.Actions;
+using FallChallenge2023.Bots.Bronze.GameMath;
 using System.Linq;
 
 namespace FallChallenge2023.Bots.Bronze.Agents.Decisions
@@ -9,14 +10,18 @@ namespace FallChallenge2023.Bots.Bronze.Agents.Decisions
         {
         }
 
+        protected Vector GetEscapeMonserMove(Vector move)
+        {
+            foreach (var monster in State.Fishes.Where(_ => _.Status == FishStatus.SWIMMING && (_.Position - Agent.Drone.Position).LengthSqr <= Drone.SCAN_RADIUS * Drone.SCAN_RADIUS))
+                return Agent.Drone.Position + monster.Speed * Drone.MAX_SPEED;
+            return move;
+        }
+
         public override GameAction GetDecision()
         {
-            //foreach (var radarBlip in Agent.Drone.RadarBlips)
-            //    if (!State.GetScannedFishes(Agent.Drone.PlayerId).Any(_ => _ == radarBlip.FishId))
-            //        return new GameActionMove(
-            //            Agent.Drone.X + Drone.MAX_SPEED * RadarBlip.GetDirectionX(radarBlip.Type), 
-            //            Agent.Drone.Y + Drone.MAX_SPEED * RadarBlip.GetDirectionY(radarBlip.Type), 
-            //            State.Turn % 3 == 0);
+            foreach (var radarBlip in Agent.Drone.RadarBlips)
+                if (!State.GetScannedFishes(Agent.Drone.PlayerId).Any(_ => _ == radarBlip.FishId))
+                    return new GameActionMove(GetEscapeMonserMove(Agent.Drone.Position + Drone.MAX_SPEED * RadarBlip.GetDirection(radarBlip.Type)), State.Turn % 3 == 0);
 
             return null;
         }
--- FallChallenge2023/Bots/Bronze/GameMath/Vector.cs
+++ FallChallenge2023/Bots/Bronze/GameMath/Vector.cs
@@ -5,12 +5,25 @@
         public int X { get; }
         public int Y { get; }
 
-        public Vector() { }
-        public Vector(int x, int y)
+        public Vector(int x = 0, int y = 0)
         {
             X = x;
             Y = y;
         }
+
+        public static Vector operator +(Vector a) => a;
+        public static Vector operator -(Vector a) => -1 * a;
+        public static Vector operator *(int k, Vector a) => new Vector(k * a.X, k * a.Y);
+        public static Vector operator *(Vector a, int k) => k * a;
+        public static Vector operator *(Vector a, Vector b) => new Vector(a.X * b.X, a.Y * b.Y);
+        public static Vector operator /(Vector a, int k) => new Vector(a.X / k, a.Y / k);
+        public static Vector operator /(Vector a, Vector b) => new Vector(a.X / b.X, a.Y / b.Y);
+        public static Vector operator +(Vector a, Vector b) => new Vector(a.X + b.X, a.Y + b.Y);
+        public static Vector operator -(Vector a, Vector b) => new Vector(a.X - b.X, a.Y - b.Y);
+
+        public static int Dot(Vector a, Vector b) => a.X * b.X + a.Y * b.Y;
+
+        public int LengthSqr => X * X + Y * Y;
 
         public override string ToString() => string.Format("({0}, {1})", X, Y);
     }
