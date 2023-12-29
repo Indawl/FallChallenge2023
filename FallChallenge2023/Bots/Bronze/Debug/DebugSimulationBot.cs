2d7b5b92c7ec57cce0db91e3d77c4c9109a06791
Indawl <Indawl@INDAWLLAND>
2023-12-29 21:09:42 +0300

    1

---
+++ FallChallenge2023/Bots/Bronze/Debug/DebugSimulationBot.cs
@@ -0,0 +1,34 @@
+﻿using DebugUtils.Objects;
+using FallChallenge2023.Bots.Bronze.Agents;
+using System.Collections.Generic;
+
+namespace FallChallenge2023.Bots.Bronze.Debug
+{
+    public class DebugSimulationBot : SimulationBot
+    {
+        public int DebugSimulation(out DebugSimulationState debugState, GameState state, List<DroneAgent> agents)
+        {
+            debugState = new DebugSimulationState("Turn " + state.Turn, state);
+            var stepState = debugState as DebugState;
+
+            Agents = agents;
+
+            var referee = new GameReferee((GameState)state.Clone());
+
+            while (!referee.IsGameOver())
+            {
+                FindActions(referee.State);
+                referee.SetNextState(Agents);
+
+                var newDebugState = new DebugState("Turn " + referee.State.Turn, (GameState)referee.State.Clone(), debugState);
+                stepState.NextMoves.Add(newDebugState);
+                stepState = newDebugState;
+            }
+            referee.Finish();
+
+            stepState.NextMoves.Add(new DebugState("Finish", referee.State, debugState));
+
+            return referee.State.Score;
+        }
+    }
+}
---
+++ FallChallenge2023/Bots/Bronze/Debug/DebugSimulationState.cs
@@ -0,0 +1,16 @@
+﻿using DebugUtils.Objects;
+using DebugUtils.Controls.Properties;
+using DevLib.Game;
+
+namespace FallChallenge2023.Bots.Bronze.Debug
+{
+    public class DebugSimulationState : DebugState, IPropertiesDetailsAction
+    {
+        public DebugSimulationState(string name, IGameState state, DebugState parent = null) : base(name, state, parent) { }
+
+        public void GetDetails()
+        {
+            (new GameManager(this)).Show();
+        }
+    }
+}
